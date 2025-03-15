079620fba8deac93a9cb97c4a0f09545d781ee81
yzbai at Wuxi <yzbai@brlite.com>
2025-03-15 17:21:29 +0800

    add

--- NewsInCSharp/Program.cs
+++ NewsInCSharp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //使用SourceGenerator 生成，避免使用Reflect发现类
+            //使用SourceGenerator 生成，避免使用Reflect发现类.
             RunableClassCollector.RunableInstances.ForEach(runable => runable.Run());
         }
     }
